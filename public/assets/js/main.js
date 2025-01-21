4643cb1c9339d87e90af6696bdfe2cef2d7056b0
Aoi719 <n1sh1_works@yahoo.co.jp>
2025-01-21 23:28:18 +0900

    カテゴリページ

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -43,14 +43,16 @@ $(function () {
   $(".mh").matchHeight();
 
   // MVスライダー
-  new Splide(".splide", {
-    type: "loop",
-    autoplay: "auto",
-    perPage: 1,
-    resetProgress: false,
-    pauseOnHover: false,
-    arrows: false,
-  }).mount();
+  if ($(".splide").length) {
+    new Splide(".splide", {
+      type: "loop",
+      autoplay: "auto",
+      perPage: 1,
+      resetProgress: false,
+      pauseOnHover: false,
+      arrows: false,
+    }).mount();
+  }
 
   // トップに戻るボタン
   $(window).on("scroll", function () {
